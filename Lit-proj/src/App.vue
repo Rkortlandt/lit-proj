17d2127e85256877e2d245283d0722ee0f17ef95
Rkortlandt <93385295+Rkortlandt@users.noreply.github.com>
2023-05-11 13:36:05 +0000

    fix styleing

--- Lit-proj/src/App.vue
+++ Lit-proj/src/App.vue
@@ -42,12 +42,12 @@ export default {
 }
 </script>
 
-<template class="h-full">
+<template class="h-full w-full">
   <div>
-    <div class="flex">
-        <div v-if="menu" class="flex flex-col bg-slate-200 w-fit items-start">
+    <div class="flex w-full">
+        <div class="transform top-0 left-9 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30" :class="menu ? 'translate-x-0' : '-translate-x-full'">
           <!-- Nav enabled -->
-          <button @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
+          <button class="max-sm:block hidden" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
           <a href="#/" class="nav-button">Home</a>
           <a href="#/Author" class="nav-button">Author</a>
           <a href="#/Plot" class="nav-button">Plot</a>
@@ -56,12 +56,13 @@ export default {
           <a href="#/Symbolism" class="nav-button">Symbolism</a>
           <a href="#/Theme" class="nav-button">Theme</a>
         </div>
-        <div v-else class="flex flex-col sm:h-screen bg-slate-200 w-10 items-start">
+        <div class="flex flex-col h-screen max-sm:hidden bg-slate-200 w-10 items-center z-40">
           <!-- Nav Disabled -->
-          <button @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
+          <button class="CollapsedSideNavButton my-1 bg-emerald-500 px-1 pt-1 rounded-lg" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
         </div>
     <div class="flex flex-col h-full">
-        <div class="flex">
+        <div class="flex px-1 w-full">
+          <button class="nav-button sm:hidden" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
           <a href="#/" class="nav-button">Home</a>
           <a href="#/Author" class="nav-button">Author</a>
           <a href="#/Plot" class="nav-button">Plot</a>
