641f5cf88efbc1ba1eaf0af3fb87b76fcf513bb5
Rkortlandt <93385295+Rkortlandt@users.noreply.github.com>
2023-05-19 15:56:59 +0000

    idk

--- Lit-proj/src/App.vue
+++ Lit-proj/src/App.vue
@@ -42,38 +42,36 @@ export default {
 }
 </script>
 
-<template class="h-full w-full">
-  <div>
-    <div class="flex w-full">
-        <div class="transform top-0 left-9 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30" :class="menu ? 'translate-x-0' : '-translate-x-full'">
-          <!-- Nav enabled -->
-          <button class="max-sm:block hidden" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
-          <a href="#/" class="nav-button">Home</a>
-          <a href="#/Author" class="nav-button">Author</a>
-          <a href="#/Plot" class="nav-button">Plot</a>
-          <a href="#/Characters" class="nav-button">Characters</a>
-          <a href="#/Setting" class="nav-button">Setting</a>
-          <a href="#/Symbolism" class="nav-button">Symbolism</a>
-          <a href="#/Theme" class="nav-button">Theme</a>
-        </div>
-        <div class="flex flex-col h-screen max-sm:hidden bg-slate-200 w-10 items-center z-40">
-          <!-- Nav Disabled -->
-          <button class="CollapsedSideNavButton my-1 bg-emerald-500 px-1 pt-1 rounded-lg" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
-        </div>
-    <div class="flex flex-col h-full">
-        <div class="flex px-1 w-full">
-          <button class="nav-button sm:hidden" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
-          <a href="#/" class="nav-button">Home</a>
-          <a href="#/Author" class="nav-button">Author</a>
-          <a href="#/Plot" class="nav-button">Plot</a>
-          <a href="#/Characters" class="nav-button">Characters</a>
-          <a href="#/Setting" class="nav-button">Setting</a>
-          <a href="#/Symbolism" class="nav-button">Symbolism</a>
-          <a href="#/Theme" class="nav-button">Theme</a>
-      </div>
-      <component :is="currentView" />
+<template class="h-full w-screen">
+  <div class="flex w-screen">
+    <div class="transform top-0 left-9 w-64 bg-slate-300 py-2 px-2 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30" :class="menu ? 'translate-x-0' : '-translate-x-full'">
+      <!-- Nav enabled -->
+      <button class="max-sm:hidden block" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
+      <a href="#/" class="nav-button">Home</a>
+      <a href="#/Author" class="nav-button">Author</a>    
+      <a href="#/Plot" class="nav-button">Plot</a>
+      <a href="#/Characters" class="nav-button">Characters</a>
+      <a href="#/Setting" class="nav-button">Setting</a>
+      <a href="#/Symbolism" class="nav-button">Symbolism</a>
+      <a href="#/Theme" class="nav-button">Theme</a>
     </div>
-  </div>
-  
+    <div class="flex flex-col h-screen bg-slate-200 w-fit items-center z-40 sticky top-0 left-0">
+      <!-- Nav Disabled -->
+      <button class="CollapsedSideNavButton py-2 max-sm:block hidden px-2" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
+      <button class="CollapsedSideNavButton py-2 max-sm:hidden block px-2" @click="menu = !menu"><span class="material-symbols-outlined">settings</span></button>
+    </div>
+    <div class="h-full">
+      <div class="flex px-1 w-full sticky top-0 z-10">
+        <button class="nav-button sm:hidden" @click="menu = !menu"><span class="material-symbols-outlined">menu</span></button>
+        <a href="#/" class="nav-button">Home</a>
+        <a href="#/Author" class="nav-button">Author</a>
+        <a href="#/Plot" class="nav-button">Plot</a>
+        <a href="#/Characters" class="nav-button">Characters</a>
+        <a href="#/Setting" class="nav-button">Setting</a>
+        <a href="#/Symbolism" class="nav-button">Symbolism</a>
+        <a href="#/Theme" class="nav-button">Theme</a>
+      </div>
+      <component :is="currentView" class="flex-grow w-full h-screen"/>
+    </div>
   </div>
 </template>
--- Lit-proj/src/components/Author.vue
+++ Lit-proj/src/components/Author.vue
@@ -2,9 +2,38 @@
 
 </script>
 
-<template>
-  <p>This is the Author page</p>
-</template>
+<template class="bg-white">
+  <p class="card-bg-title">Neal Shusterman</p>
+  <p class="text-4xl w-full text-center py-5 my-20 underline">When he was young he wanted to do everything...</p>
+  <p class="card-bg">“I had a teacher who said 'You can’t do that you’ll be a jack of all trades and a master of none!' <br> But I had it worked out: I’d be a jack of seven trades, and master of three.”</p>
+  <p class="no-card">In ninth grade he had an English teacher who changed his life. He was challenged to write a book every month for extra credit. That year he realized that writing was a true passion. Neal Shusterman has written over 30 books as an author and doesn't plan to stop. He has won many awards such as the National Book Award for Young People’s Literature for his book Challenger Deep. He lived in brooklyn till he was 16 when his family moved to mexico city. He attended the University of California, Irvine and double majored in theater and psychology.</p>
+  <p class="card-bg-subtitle">Awards</p>
+  <p class="no-card-left-md">
+    Bruiser <br>
+    <span class="px-5"></span>  2012 Eliot Rosewater Indiana High School Book Award -- Grades 9-12 <br>
+    Challenger Deep <br>
+    <span class="px-5"></span>  2015 National Book Award -- Young People's Literature <br>
+    Scythe <br>
+    <span class="px-5"></span>  2020 California Young Reader Medal -- Young Adult <br>
+    Tesla's Attic <br>
+    <span class="px-5"></span>  2016 Beehive Award -- Young Adult <br>
+    Unwind <br>
+    <span class="px-5"></span>  2009 Black-Eyed Susan Award -- High School <br>
+    <span class="px-5"></span>  2009 Eliot Rosewater Indiana High School Book Award -- Grades 9-12 <br>
+    <span class="px-5"></span>  2010 Evergreen Young Adult Book Award -- Young Adult <br>
+    <span class="px-5"></span>  2010 Green Mountain Book Award -- Grades 9-12 <br>
+    <span class="px-5"></span>  2010 Nevada Young Readers' Award -- Intermediate <br>
+    <span class="px-5"></span>  2010 Sequoyah Book Award -- Intermediate <br>
+    <span class="px-5"></span>  2010 Virginia Readers Choice Award -- High School <br>
+    <span class="px-5"></span>  2011 Nutmeg Book Award -- Teen <br>
+  </p>
+  <p class="card-bg-subtitle">Books By Shusterman</p>
+
+  <div class="flex overflow-auto w-full">
+    <img src="./">
+  </div>
+
+</template> 
 
 <style scoped>
 
