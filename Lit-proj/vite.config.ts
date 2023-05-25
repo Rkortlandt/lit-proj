247a2ef090308018fe37d855ec724de1a7955e29
Rkortlandt <93385295+Rkortlandt@users.noreply.github.com>
2023-05-25 01:24:14 +0000

    idk

--- Lit-proj/vite.config.ts
+++ Lit-proj/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [vue()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/lit-proj/'
+  }
+
+  return config
 })
